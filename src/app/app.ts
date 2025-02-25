884b1e9c3aab9a8355b4d56bcf682bae9da667ed
hafij <hafij.alasad23@gmail.com>
2025-02-26 00:33:54 +0600

    feat: json and text parser used for data parsing in express

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
-import express from 'express';
-const app = express()
+import express, { Request, Response } from 'express';
+const app = express();
+
+//parsers
+
+app.use(express.json());
+app.use(express.text());
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello my World!')
+})
+
+app.post('/', (req: Request, res: Response)=>{
+    console.log(req.body);
+    res.send(JSON.stringify({data: req.body}))
 })
 
 export default app;
